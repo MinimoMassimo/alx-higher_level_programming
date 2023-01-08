6d8bf7c4beea39b43b95a3cb00c55676dae6cc81
MBN <milkabiniam@gmail.com>
2023-01-08 07:50:00 +0300

    7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python
 def add_tuple(tuple_a=(), tuple_b=()):
-    new_tuple
-    for i in range(2):
-        new_tuple =(new_tuple, tuple_a[i] + tuple_b[i])
+    new_tuple = []
+    if len(tuple_a) == 0:
+        tuple_a += (0,0)
+    elif len(tuple_a) == 1:
+        tuple_a += (0,)
 
-    return new_tuple
+    if len(tuple_b) == 0:
+        tuple_b += (0,0)
+    elif len(tuple_b) == 1:
+        tuple_b += (0,)
+
+    for i in range(2):
+        new_tuple.append(tuple_a[i] + tuple_b[i])
+
+    return tuple(new_tuple)
