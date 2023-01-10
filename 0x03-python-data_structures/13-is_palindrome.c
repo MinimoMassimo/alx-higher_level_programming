76fd1cea64a2fc0c7dfbac490665278e2acf53b3
MBN <milkabiniam@gmail.com>
2023-01-10 10:38:11 +0300

    13 is more efficient

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -11,7 +11,7 @@
 int is_palindrome(listint_t **head)
 {
 	listint_t *temp, *main;
-	int len = 1;
+	int len = 1, temp_len = 0;
 	int i = 0;
 
 	if (head == NULL || *head == NULL)
@@ -24,20 +24,27 @@ int is_palindrome(listint_t **head)
 		len++;
 	}
 	
-	for (; len >= 0; len--)
+	temp_len = len - 1;
+	if (len % 2 == 0)
+		len /= 2;
+	else
+		len = (len -1) / 2;
+	for (; len > 0; len--)
 	{
-		for (i = 0; temp->next && i < len - 1; i++)
+		
+		for (i = 0; temp->next && i < temp_len; i++)
 			temp = temp->next;
-		/*
+		
 		printf("main:%d ", main->n);
 		printf("temp:%d ", temp->n);
-		*/
+		
 		if (main->n != temp->n)
 			return (0);
 		
 		if (main->next)
 		       main = main->next;
 		temp = *head;
+		temp_len--;
 	}
 
 	return (1);
