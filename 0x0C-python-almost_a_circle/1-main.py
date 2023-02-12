b665ed6eac8a593aa80a11172f020d5e7c1015e7
MBN <milkabiniam@gmail.com>
2023-02-12 09:32:56 +0300

    til 6

--- 0x0C-python-almost_a_circle/1-main.py
+++ 0x0C-python-almost_a_circle/1-main.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """ 1-main """
 from models.rectangle import Rectangle
-
+from models.base import Base
 if __name__ == "__main__":
 
-    r1 = Rectangle(10, 2)
+    r1 = Rectangle(10,2)
     print(r1.id)
 
     r2 = Rectangle(2, 10)
--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -4,7 +4,7 @@
 from models.base import Base
 
 
-def Rectangle(Base):
+class Rectangle(Base):
     '''defines a rectangle and inherits from Base
         Attributes:
             __width
@@ -16,11 +16,27 @@ def Rectangle(Base):
             getters and setters for all attributes
     '''
 
-
     def __init__(self, width, height, x=0, y=0, id=None):
         '''initializes
         '''
         super().__init__(id)
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if not isinstance(x, int):
+            raise TypeError("x must be an integer")
+        if not isinstance(y, int):
+            raise TypeError("y must be an integer")
+        if width <= 0:
+            raise ValueError("width must be > 0")
+        if height <= 0:
+            raise ValueError("height must be > 0")
+        if x < 0:
+            raise ValueError("x must be >= 0")
+        if y < 0:
+            raise ValueError("y must be >= 0")
+
         self.width = width
         self.height = height
         self.x = x
@@ -47,19 +63,53 @@ def Rectangle(Base):
         '''
         return self.__y
 
+    @width.setter
     def width(self, val):
         '''setter
         '''
+        if not isinstance(val, int):
+            raise TypeError("width must be an integer")
+        if val <= 0:
+            raise ValueError("width must be > 0")
         self.__width = val
+    @height.setter
     def height(self, val):
         '''setter
         '''
+        if not isinstance(val, int):
+            raise TypeError("height must be an integer")
+        if val <= 0:
+            raise ValueError("height must be > 0")
         self.__height = val
+    @x.setter
     def x(self, val):
         '''setter
         '''
+        if type(val) is not int:
+            raise TypeError("x must be an integer")
+        if val < 0:
+            raise ValueError("x must be >= 0")
         self.__x = val
+    @y.setter
     def y(self, val):
         '''setter
         '''
+        if not isinstance(val, int):
+            raise TypeError("y must be an integer")
+        if val < 0:
+            raise ValueError("y must be >= 0")
         self.__y = val
+
+    def area(self):
+        '''returns area
+        '''
+        return self.__height * self.__width
+
+    def display(self):
+        '''prints in stdout theRectablge instance with '#'
+        '''
+        for i in range(self.__height):
+            print('#'*self.__width)
+
+    def __str__(self):
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x, self.__y, self.__width, self.__height)
