8d1dd454552665cb6ab98f33907f4fdbc03b854a
MBN <milkabiniam@gmail.com>
2023-04-10 18:09:04 +0300

    l

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,6 +3,6 @@ const args = process.argv.slice(2).map(Number);
 if (args.length === 0 || args.length === 1) {
   console.log(0);
 } else {
-  args.sort(function(a, b){return a - b});  
+  args.sort(function (a, b) { return a - b; });
   console.log(args[args.length - 2]);
 }
