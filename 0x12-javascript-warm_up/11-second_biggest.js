71065ffdcfacddd43b9615ed3a62827244be876d
MBN <milkabiniam@gmail.com>
2023-04-10 18:08:20 +0300

    l

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,13 +3,6 @@ const args = process.argv.slice(2).map(Number);
 if (args.length === 0 || args.length === 1) {
   console.log(0);
 } else {
-  let Max = args[0];
-  let sol = Max;
-  for (let i = 0; i < args.length; i++) {
-    if (args[i] > Max) Max = args[i];
-  }
-  for (let i = 0; i < args.length; i++) {
-    if (args[i] > sol && args[i] < Max) sol = args[i];
-  }
-  console.log(sol);
+  args.sort(function(a, b){return a - b});  
+  console.log(args[args.length - 2]);
 }
