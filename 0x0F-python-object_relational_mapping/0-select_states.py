d7c3e2046e4000b461e25a686cb9087fdd234ff0
MBN <milkabiniam@gmail.com>
2023-04-16 16:13:42 +0300

    l

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+return all states from database 'hbtn_0e_0_usa
+parameters passed: username, password, database
+"""
+
+
+import MySQLdb
+from sys import argv
+
+if __name__=="__main__":
+    #connect to database:
+    DB = MySQLdb.connect(host='localhost', port=3306,
+            user=argv[1], passwd=argv[2],
+            db=argv[3])
+
+    #create cursor to execute SQL queries
+    cur = DB.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    for x in cur.fetchall():
+        print(x)
+        cur.close()
+        DB.close()
